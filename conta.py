16884c4ac8b75d7d2bb3f4572452aab2e9ef22f9
Edicley <edyhenrique@gmail.com>
2020-03-09 14:55:50 -0300

    Criando metódos extrado, deposita e saca.

--- conta.py
+++ conta.py
@@ -8,4 +8,11 @@ class Conta:
         self.saldo = saldo
         self.limite = limite
 
+    def extrato(self):
+        print(f"Saldo de {self.saldo} do titular {self.titular}")
 
+    def deposita(self, valor):
+        self.saldo += valor
+
+    def saca(self, valor):
+        self.saldo -= valor
